c310483b0a56669560ccc800ef7132ded1d3d9d4
andy <andrew.w.pinson@gmail.com>
2020-09-05 19:58:47 -0700

    Making mene my bitch!  He couldn't figure this shit out

--- facet-ninja.js
+++ facet-ninja.js
@@ -1,16 +1,23 @@
 var all = document.getElementsByTagName("*");
 const getFacets = () => {
-    fetch('https://api.facet.ninja/facet/mene-prod', {
+    fetch(`https://api.facet.ninja/facet/${window.location.hostname}`, {
     }).then(function (response) {
         return response.json();
     }).then(function (data) {
         const { facet } = data;
         facet && facet.forEach(f => {
             f.id && f.id.forEach(idx => {
-                var elem = document.querySelector(`#${idx}`);
-                if (elem) elem.style.display = 'none';
+                //var elem = document.querySelector(`#${idx}`);
+                //if (elem) elem.style.display = 'none';
+                var node = $(idx)[0]
+                node.remove();
             })
         })
     });
 }
 getFacets();
+
+//var node = $("body#fixed-container > div#welcome")[0]
+//node.remove();
+
+
